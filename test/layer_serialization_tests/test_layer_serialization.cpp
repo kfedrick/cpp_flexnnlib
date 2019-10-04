5a1feed2a73ed92eb890bfb76c1e6d0d7e69952b
kfedrick <Alonzo.Fedrickj@gmail.com>
2019-10-04 16:23:50 -0600

    First round update to Layer class hierarchy and serializers.

---
+++ test/layer_serialization_tests/test_layer_serialization.cpp
@@ -0,0 +1,15 @@
+//
+// Created by kfedrick on 6/17/19.
+//
+
+
+#include <gtest/gtest.h>
+
+#include "test_layer_serialization.h"
+
+int main (int argc, char **argv)
+{
+   ::testing::InitGoogleTest (&argc, argv);
+
+   return RUN_ALL_TESTS ();
+}
